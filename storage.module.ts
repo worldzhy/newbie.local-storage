372033ee957a1070d6bddc9588e8467df1fcf900
worldzhy <worldzhy@126.com>
2024-08-30 12:28:15 +0800

    remove s3 feature because of dulplicate

--- storage.module.ts
+++ storage.module.ts
@@ -2,7 +2,6 @@ import {Global, Module} from '@nestjs/common';
 import {LocalDriveService} from './local/local-drive.service';
 import {GoogleDriveService} from './google-drive/google-drive.service';
 import {GoogleDrivePermissionService} from './google-drive/google-drive-permission.service';
-import {S3Service} from './s3/s3.service';
 
 @Global()
 @Module({
@@ -10,13 +9,11 @@ import {S3Service} from './s3/s3.service';
     LocalDriveService,
     GoogleDriveService,
     GoogleDrivePermissionService,
-    S3Service,
   ],
   exports: [
     LocalDriveService,
     GoogleDriveService,
     GoogleDrivePermissionService,
-    S3Service,
   ],
 })
 export class StorageModule {}
